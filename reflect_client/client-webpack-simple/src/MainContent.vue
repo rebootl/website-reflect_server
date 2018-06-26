b8677011897864c0d4f94f293c6ad627d1cfd824
Cem Aydin <cem.aydin@gmx.ch>
2018-06-26 22:51:20 +0200

    add content type tabs (WIP), noscript tag

--- reflect_client/client-webpack-simple/src/MainContent.vue
+++ reflect_client/client-webpack-simple/src/MainContent.vue
@@ -1,14 +1,24 @@
 <template>
-  <main id="main-content">
-    text, blablabla {{ this.content_data.query_str }}
-  </main>
+  <div id="main-content-container">
+    <nav id="content-types-tabs">
+      <ul>
+        <li>All <span class="types-num">10</span></li>
+        <li>Notes <span class="types-num">3</span></li>
+        <li>Links <span class="types-num">5</span></li>
+        <li>Images <span class="types-num">2</span></li>
+      </ul>
+    </nav>
+    <main id="main-content">
+      text, blablabla {{ this.content_data.query_str }}
+    </main>
+  </div>
 </template>
 
 <script>
 const get_content_data_url = "http://hplaptop:5010/api/get_content_data";
 
 export default {
-  name: 'MainContents',
+  name: 'MainContent',
   props: [ "selection_data" ],
   watch: {
     selection_data: {
@@ -66,14 +76,43 @@ export default {
 
 <style lang="less">
 @import (reference) "./globals.less";
-main {
+#main-content-container {
   margin-left: 210px;
   margin-top: 43px;
   max-width: 550px;
+}
+#content-types-tabs {
+  height: 35px;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    height: 30px;
+    li {
+      position: relative;
+      padding-left: 10px;
+      box-sizing: border-box;
+      padding-top: 7px;
+      width: calc(100% / 4);
+      height: 30px;
+      display: block;
+      float: left;
+      border: 1px dashed orange;
+      .types-num {
+        position: absolute;
+        right: 5px;
+        top: 4px;
+        font-weight: bold;
+        font-size: 1.2em;
+      }
+    }
+  }
+}
+main {
   min-height: 300px;
   padding-left: 30px;
   padding-right: 30px;
-  padding-top: 3px;
+  padding-top: 20px;
   background-color: @main-content-bg;
 }
 </style>
