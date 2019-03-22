be074ebb9a7e96fecdd9b961c82eb168bc2360d9
Cem Aydin <cem.aydin@gmx.ch>
2019-03-22 14:27:08 +0100

    add routing (working WIP)

--- client/src/reflect-app/reflect-app.js
+++ client/src/reflect-app/reflect-app.js
@@ -1,5 +1,6 @@
 import { html, render } from 'lit-html';
 import { global_state } from './global_state.js';
+import './router.js';
 import './main-header.js';
 
 const style = html`
@@ -28,15 +29,52 @@ const style = html`
   </style>
 `;
 
+const routes = {
+  'entries': (params) => html`<entries params=${params}></entries>`,
+  'add-entry': () => html`<add-entry></add-entry>`,
+  'edit-entry': () => html`<edit-entry></edit-entry>`,
+  'edit-topic': () => html`<edit-topic></edit-topic>`,
+};
+
+/* handle params in subcomp.
+const params = new URLSearchParams(params_str);
+for (let p of params) {
+  console.log(p);
+}
+console.log(params.getAll('foo'));
+*/
+
 class ReflectApp extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
+
+    // listen for url changes
+    window.addEventListener('hashchange', ()=>this.urlChange());
+    window.addEventListener('load', ()=>this.urlChange());
+
+    //this.update();
+  }
+  urlChange() {
+    const hash_str = location.hash.slice(1) || '';
+    let ref, params_str;
+    if (hash_str.includes('?')) {
+      [ ref, params_str ] = hash_str.split('?');
+    } else {
+      ref = hash_str;
+      params_str = "";
+    }
+    if (!routes.ref) { ref = 'entries'; }
+    this.routed_content = routes[ref](params_str);
     this.update();
   }
   update() {
     render(html`${style}
         <main-header></main-header>
+        <div id="main-content">
+          <main-menu></main-menu>
+          ${this.routed_content}
+        </div>
       `
       , this.shadowRoot);
   }
---
+++ client/src/reflect-app/router.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+function url_change() {
+
+  console.log(location);
+
+  const hash = location.hash
+
+}
+
+
+
+//window.addEventListener('hashchange', url_change);
+//swindow.addEventListener('load', url_change);
